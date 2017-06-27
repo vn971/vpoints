c04a53604ac943e3e67918de229c4a279f3691da
Vasya Novikov <n1dr+cm3513git@ya.ru>
2017-06-27 15:21:56 +0300

    small documentation update

--- vpoints.sh
+++ vpoints.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+# launches VPoints on linux
 
-# launch VPoints from linux ("wine" must be installed)
+# Prequisities:
+# 1. wine
+# 2. CP1251 locale
+
+# Install CP1251 on Debian derivatives (Ubuntu, Mint,..):
+#     echo "ru_RU CP1251" >> /var/lib/locales/supported.d/ru
+#     dpkg-reconfigure locales
+
+# Install CP1251 on ArchLinux:
+#     echo 'ru_RU.CP1251 CP1251' >> /etc/locale.gen
+#     local-gen
 
 LANG=ru_RU.CP1251 LC_ALL=ru_RU.CP1251 wine vpoints.exe
